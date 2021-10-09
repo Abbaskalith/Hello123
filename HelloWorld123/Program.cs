2c5f714aa99b6706152c2dc974a54fc337de1163
Abbaskalith <abkalith@gmail.com>
2021-10-09 22:48:47 +0530

    HelloWorld123 Created

---
+++ HelloWorld123/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(12 + 12 );
+        }
+    }
+}
